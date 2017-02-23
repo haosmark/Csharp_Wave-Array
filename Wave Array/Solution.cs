597c3ba82128175da75bfb3cd39b1c8877abfd0b
Denis St <haosmark@gmail.com>
2017-02-23 11:23:25 -0500

    Add project files.

---
+++ Wave Array/Solution.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * Problem description: Given an array of integers, sort the array into a 
+ * wave like array and return it, in other words, arrange the elements into 
+ * a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+ */
+
+using System.Collections.Generic;
+
+namespace Wave_Array
+{
+    public class Solution
+    {
+        public static List<int> WaveArray(List<int> A)
+        {
+            // make sure the collection is sorted
+            A.Sort();
+
+            // swap evens with odds in the list
+            for (int i = 0; i < A.Count; i += 2)
+            {
+                swap(A, i, i + 1);
+            }
+
+            return A;
+        }
+
+        ///<summary>swaps given indexes in the List collection</summary>
+        ///<param name="data">List collection on which the operation is to be performed</param>  
+        ///<param name="a">First swap index</param>
+        ///<param name="b">Second swap index</param>
+        private static void swap(List<int> data, int a, int b)
+        {
+            if (b < data.Count)
+            {
+                int c = data[b];
+                data[b] = data[a];
+                data[a] = c;
+            }
+        }
+    }
+}
---
+++ WaveArray.Tests/SolutionTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Wave_Array;
+
+namespace WaveArray.Tests
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            // Arrange
+            List<int> data = new List<int> { 1 };
+            List<int> expected = new List<int> { 1 };
+
+            // Act
+            List<int> actual = Solution.WaveArray(data);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_1_2_3()
+        {
+            // Arrange
+            List<int> data = new List<int> { 1, 2, 3 };
+            List<int> expected = new List<int> { 2, 1, 3 };
+
+            // Act
+            List<int> actual = Solution.WaveArray(data);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_6_2_3_4()
+        {
+            // Arrange
+            List<int> data = new List<int> { 6, 2, 3, 4 };
+            List<int> expected = new List<int> { 3, 2, 6, 4 };
+
+            // Act
+            List<int> actual = Solution.WaveArray(data);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
